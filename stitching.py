dd5fc476d687593d97809a5f4172be38e07ed34c
Ryheff24 <40723346+Ryheff24@users.noreply.github.com>
2026-03-19 16:28:58 -0400

    added code to swap dst and src as needed started working on removing these people from the image since regular stitching wont work.

--- stitching.py
+++ stitching.py
@@ -5,7 +5,6 @@ Notes:
 3. If you want to show an image for debugging, please use show_image() function in util.py. 
 4. Please do NOT save any intermediate files in your final submission.
 '''
-from matplotlib.pyplot import show
 import torch
 import kornia as K
 from typing import Dict
@@ -31,6 +30,7 @@ def stitch_background(imgs: Dict[str, torch.Tensor]):
     imlist = list(imgs.values())
     left = (imlist[0]/255).unsqueeze(0)
     right = (imlist[1]/255).unsqueeze(0)
+    
     # padding = abs(left.shape[2] - right.shape[2])
     # pad = (110,)*4
     # left_pad = torch.nn.functional.pad(left, (0,0,0,padding))
@@ -41,14 +41,22 @@ def stitch_background(imgs: Dict[str, torch.Tensor]):
       "image0": K.color.rgb_to_grayscale(left),
       "image1": K.color.rgb_to_grayscale(right)
     }
-
+    
+    # inputdict = {
+    #   "image0": K.color.rgb_to_grayscale(right),
+    #   "image1": K.color.rgb_to_grayscale(left)
+    # }
+    
     loftr = K.feature.LoFTR(pretrained='outdoor')
     IS = K.contrib.ImageStitcher(loftr, estimator='ransac')
+    
     points = loftr(inputdict)
     ransac = K.geometry.RANSAC() 
     homo = ransac(points["keypoints0"], points["keypoints1"])
+    
     shp = left.shape
-    # print(shp)
+    # shp = right.shape
+    
     def transform(points, H):
         # Equation 2.21 Szeliski Computer Vision: Algorithms and Applications
         out = []
@@ -61,6 +69,7 @@ def stitch_background(imgs: Dict[str, torch.Tensor]):
             out.append((int(x_prime.round()), int(y_prime.round())))
         return tuple(out)
     # print(shp)
+    
     tl,tr = (0, 0), (shp[3],0)
     bl, br = (0,shp[2]), (shp[3], shp[2])
     points = (tl, tr, bl, br)
@@ -69,19 +78,22 @@ def stitch_background(imgs: Dict[str, torch.Tensor]):
     
     x = tl_prim[0], tr_prim[0], bl_prim[0], br_prim[0], right.shape[3], 0
     y = tl_prim[1], tr_prim[1], bl_prim[1], br_prim[1], right.shape[2], 0
+    
+    # x = tl_prim[0], tr_prim[0], bl_prim[0], br_prim[0], left.shape[3], 0
+    # y = tl_prim[1], tr_prim[1], bl_prim[1], br_prim[1], left.shape[2], 0
+    
+    
     minx, maxx = int(min(x)), int(max(x))
     miny, maxy = int(min(y)), int(max(y))
-            
-        
+    
     width = maxx-minx
     height = maxy-miny
     outs = (height, width)
     
-  
     # homo needs transform
     T = torch.tensor([[1, 0, -minx],[0, 1 ,-miny],[0, 0, 1]], dtype=torch.float32)
-    # H = T @ homo[0].unsqueeze(0)
     H = (T @ homo[0]).unsqueeze(0)
+    
     # print(homo[0].shape, H.shape, T.shape)
     # print("T:",T)
     # print("homo:", homo[0])
@@ -89,21 +101,70 @@ def stitch_background(imgs: Dict[str, torch.Tensor]):
     
     # good stuff
     src_img = K.geometry.warp_perspective(left, H, outs)
+    # src_img = K.geometry.warp_perspective(right, H, outs)
+    
     # show_image(src_img.squeeze())
-    padding = (height-right.shape[2])//2
+    # padding = (height-right.shape[2])//2
     # https://docs.pytorch.org/docs/stable/generated/torch.nn.functional.pad.html 
     # then use (padding_left,padding_right, padding_top,padding_bottom)
-    #(at least minx,the width + minx, miny, the height + miny)
-    print(minx,width+minx, miny, height+miny)
+    # (at least minx,the width + minx, miny, the height + miny)
+    # print(minx,width+minx, miny, height+miny)
     
     minx = abs(minx)
     miny = abs(miny)
-    print(f"minx: {minx}\nwidth+minx: {width+minx}\nminy: {miny}\nheight+miny: {height+miny}\nright.shape: {right.shape}\nsrc_img.shape: {src_img.shape}\nright.shape[2]-height+miny: {right.shape[2]-height+miny} ")
-    right = torch.nn.functional.pad(right, (minx,right.shape[3]-width+minx, miny, height-miny-right.shape[2]))     
-
-    # dst_img = torch.concatenate([right, torch.zeros_like(left_pad)], -1)
-    write_image((src_img.squeeze(0) * 255).to(torch.uint8), "src.png")
-    write_image((right.squeeze(0) * 255).to(torch.uint8), "dst.png")
+    
+    # print(f"minx: {minx}\nwidth+minx: {width+minx}\nminy: {miny}\nheight+miny: {height+miny}\nright.shape: {right.shape}\nsrc_img.shape: {src_img.shape}\nright.shape[2]-height+miny: {right.shape[2]-height+miny} ")
+    # print(f"minx: {minx}\nwidth+minx: {width+minx}\nminy: {miny}\nheight+miny: {height+miny}\nleft.shape: {left.shape}\nsrc_img.shape: {src_img.shape}\nleft.shape[2]-height+miny: {left.shape[2]-height+miny} ")
+    
+    dst_img = torch.nn.functional.pad(right, (minx,right.shape[3]-width+minx, miny, height-miny-right.shape[2]))     
+    # dst_img = torch.nn.functional.pad(left, (minx, width-minx-left.shape[3], miny, height-miny-left.shape[2]))     
+    dst_img = dst_img.squeeze(0)
+    src_img = src_img.squeeze(0)
+    
+    src_mask = torch.any(src_img > 0, dim=0, keepdim=True)
+    dst_mask = torch.any(dst_img > 0, dim=0, keepdim=True)
+    # print(src_mask.shape, src_mask.dtype)
+    # print(dst_mask.shape, dst_mask.dtype)
+    # src_mask = src_mask.repeat(3,1,1)
+    # dst_mask = dst_mask.repeat(3,1,1)
+    # src_mask[1] = 0
+    # src_mask[2] = 0
+    
+    def ready(tens):
+        return (tens * 255).to(torch.uint8)
+    def i8(tens):
+        return tens.to(torch.uint8)
+    
+    # combined_mask = torch.where(src_img.bool(), src_img, dst_img)
+    combined_mask = torch.where(dst_img.bool(), dst_img, src_img)
+    write_image(ready(combined_mask), "mask.png")
+    
+    both = dst_mask & src_mask
+    print(f"""
+Both: {both.shape},
+dst_img: {dst_img.shape},
+dst_mask: {dst_mask.shape},
+src_img: {src_img.shape},
+src_mask: {src_mask.shape}
+    """)
+    
+    # dst_img = dst_img
+    # src_img = src_img
+    both_dst_img = dst_img * both.float()
+    both_src_img = src_img * both.float()
+    
+    dif = abs(both_dst_img - both_src_img)
+    
+    dif = torch.threshold(dif, 0.3, 0)
+    
+    print(dif.shape)
+    
+    # dif = i8(dif)
+    write_image(ready(dif), "dif1.png")
+    # write_image(both, "both.png")
+    
+    write_image(ready(dst_img), "dst.png")
+    write_image(ready(src_img), "src.png")
     
     return img
 
