e6effe087187d2dc87ef93f0578d2ea5243ff1d9
Ryheff24 <40723346+Ryheff24@users.noreply.github.com>
2026-03-21 15:37:54 -0400

    file saving for quick param tuning, a non alpha blur that works kinda, and a alpha version that attempts to use canny.

--- stitching.py
+++ stitching.py
@@ -5,8 +5,7 @@ Notes:
 3. If you want to show an image for debugging, please use show_image() function in util.py. 
 4. Please do NOT save any intermediate files in your final submission.
 '''
-
-from matplotlib.pyplot import show
+import matplotlib.pyplot as plt
 import torch
 import kornia as K
 from typing import Dict
@@ -27,199 +26,243 @@ def stitch_background(imgs: Dict[str, torch.Tensor]):
     img = torch.zeros((3, 256, 256), dtype=torch.uint8) # assumed 256*256 resolution. Update this as per your logic.
 
     #TODO: Add your code here. Do not modify the return and input arguments.
-    # 0 t1_1 is src, t1_2 is dst, 
-    # 1 t1_2 is src, t1_1 is dst
-    variant = 1
-    
+    #Global params
+    # 0 is true
+    variant = True  # True: t1_1 is src, t1_2 is dst, False: t1_2 is src, t1_1 is dst
+    save = False
+    load = True
+    save_path = "data/"
+    # RANSAC params
+    inl_th = 1.5
+    max_iter = 100
+    confidence = 0.999
+    max_lo_iters = 15
     torch.manual_seed(1234)
-    imlist = list(imgs.values())
-    t1_1 = (imlist[0]/255).unsqueeze(0)
-    t1_2 = (imlist[1]/255).unsqueeze(0)
-    def ready(tens):
-        return (tens * 255).to(torch.uint8)
-    def i8(tens):
-        return tens.to(torch.uint8)
-    # padding = abs(t1_1.shape[2] - t1_2.shape[2])
-    # pad = (110,)*4
-    # left_pad = torch.nn.functional.pad(t1_1, (0,0,0,padding))
-    # out_shape = (max(t1_1.shape[-2], t1_2.shape[-2]), t1_1.shape[-1] + t1_2.shape[-1])
-    # print(out_shape)
-    if variant == 0:
-        inputdict = {
-        "image0": K.color.rgb_to_grayscale(t1_1),
-        "image1": K.color.rgb_to_grayscale(t1_2)
-        }
-        shp = t1_1.shape
-    else:
-        inputdict = {
-        "image0": K.color.rgb_to_grayscale(t1_2),
-        "image1": K.color.rgb_to_grayscale(t1_1)
-        }
-        shp = t1_2.shape
-    
-    loftr = K.feature.LoFTR(pretrained='outdoor')
-    # IS = K.contrib.ImageStitcher()
-    # IS.blend_image()
-    points = loftr(inputdict)
-    ransac = K.geometry.RANSAC(max_iter=20, confidence=0.999, max_lo_iters=20) 
-    homo = ransac(points["keypoints0"], points["keypoints1"])
-    
-    
-    # shp = t1_2.shape
-    
-    def transform(points, H):
-        # Equation 2.21 Szeliski Computer Vision: Algorithms and Applications
-        out = []
-        for point in points:
-            x = point[0]
-            y = point[1]
-            h = H
-            x_prime = (h[0][0]*x + h[0][1]*y + h[0][2])/(h[2][0]*x + h[2][1]*y + h[2][2])
-            y_prime = (h[1][0]*x + h[1][1]*y + h[1][2])/(h[2][0]*x + h[2][1]*y + h[2][2])
-            out.append((int(x_prime.round()), int(y_prime.round())))
-        return tuple(out)
-    # print(shp)
-    
-    tl,tr = (0, 0), (shp[3],0)
-    bl, br = (0,shp[2]), (shp[3], shp[2])
-    points = (tl, tr, bl, br)
-    t = transform(points, homo[0])
-    tl_prim, tr_prim, bl_prim, br_prim = t[0],t[1],t[2],t[3]
-    if variant == 0:
-        x = tl_prim[0], tr_prim[0], bl_prim[0], br_prim[0], t1_2.shape[3], 0
-        y = tl_prim[1], tr_prim[1], bl_prim[1], br_prim[1], t1_2.shape[2], 0
-    else:
-        x = tl_prim[0], tr_prim[0], bl_prim[0], br_prim[0], t1_1.shape[3], 0
-        y = tl_prim[1], tr_prim[1], bl_prim[1], br_prim[1], t1_1.shape[2], 0
-    
-    
-    minx, maxx = int(min(x)), int(max(x))
-    miny, maxy = int(min(y)), int(max(y))
-    
-    width = maxx-minx
-    height = maxy-miny
-    outs = (height, width)
-    
-    # homo needs transform
-    T = torch.tensor([[1, 0, -minx],[0, 1 ,-miny],[0, 0, 1]], dtype=torch.float32)
-    H = (T @ homo[0]).unsqueeze(0)
-    
-    # print(homo[0].shape, H.shape, T.shape)
-    # print("T:",T)
-    # print("homo:", homo[0])
-    # print("H:", H)
-    
-    # good stuff
+    # Mask params
     m = "bilinear" # bilinear bicubic
     p = "zeros" # "zeros" "border" "reflection" "fill"
     align_corners = True
-    
-    if variant== 0:
-        src_img = K.geometry.warp_perspective(t1_1, H, outs, padding_mode=p, align_corners=align_corners, mode=m) # change mode
-        src_mask = K.geometry.warp_perspective(torch.ones_like(t1_1), H, outs, padding_mode=p, align_corners=align_corners, mode=m) # change mode
-    else:
-        src_img = K.geometry.warp_perspective(t1_2, H, outs, padding_mode=p, align_corners=align_corners, mode=m)
-        src_mask = K.geometry.warp_perspective(torch.ones_like(t1_2), H, outs, padding_mode=p, align_corners=align_corners, mode=m)
-
-    # show_image(src_mask.squeeze(0))
-    # show_image(src_img.squeeze(0))
-    
-    # https://docs.pytorch.org/docs/stable/generated/torch.nn.functional.pad.html 
-    # then use (padding_left,padding_right, padding_top,padding_bottom)
-    # (at least minx,the width + minx, miny, the height + miny)
-    # print(minx,width+minx, miny, height+miny)
-    
-    minx = abs(minx)
-    miny = abs(miny)
-    
-    # print(f"minx: {minx}\nwidth+minx: {width+minx}\nminy: {miny}\nheight+miny: {height+miny}\nright.shape: {t1_2.shape}\nsrc_img.shape: {src_img.shape}\nright.shape[2]-height+miny: {t1_2.shape[2]-height+miny} ")
-    # print(f"minx: {minx}\nwidth+minx: {width+minx}\nminy: {miny}\nheight+miny: {height+miny}\nleft.shape: {t1_1.shape}\nsrc_img.shape: {src_img.shape}\nleft.shape[2]-height+miny: {t1_1.shape[2]-height+miny} ")
-    if variant == 0:
-        dst_img = torch.nn.functional.pad(t1_2, (minx,t1_2.shape[3]-width+minx, miny, height-miny-t1_2.shape[2]))     
-        dst_mask = torch.nn.functional.pad(torch.ones_like(t1_2), (minx,t1_2.shape[3]-width+minx, miny, height-miny-t1_2.shape[2]))     
-    else:
-        dst_img = torch.nn.functional.pad(t1_1, (minx, width-minx-t1_1.shape[3], miny, height-miny-t1_1.shape[2]))     
-        dst_mask = torch.nn.functional.pad(torch.ones_like(t1_1), (minx, width-minx-t1_1.shape[3], miny, height-miny-t1_1.shape[2]))     
-     
-    
-    
-    
-    dst_img = dst_img.squeeze(0)
-    src_img = src_img.squeeze(0)
-    dst_mask = dst_mask.squeeze(0).bool()
-    src_mask = src_mask.squeeze(0).bool()
-    
-    # src_mask = torch.any(src_img > 0, dim=0, keepdim=True)
-    # dst_mask = torch.any(dst_img > 0, dim=0, keepdim=True)
-    
-    print(src_mask.shape, src_mask.dtype)
-    print(dst_mask.shape, dst_mask.dtype)
-    # src_mask = src_mask.repeat(3,1,1)
-    # dst_mask = dst_mask.repeat(3,1,1)
-    # src_mask[1] = 0
-    # src_mask[2] = 0
-    
-
-    
-    # combined_mask = torch.where(src_mask, src_img, dst_img)
-    # combined_mask = torch.where(dst_mask.bool(), dst_img, src_img)
-        
+    if save:
             
-    
-    both = dst_mask & src_mask
-    both_dst_img = dst_img * both.float()
-    both_src_img = src_img * both.float()
-    dif = abs(both_src_img - both_dst_img)
-    
-    sigma = 8
-    filter_size = 6 * sigma + 1
-    erosion_kernel = 10
-    dilation_kernel = 75
-
-    dst_dif = both_dst_img - both_src_img 
-    src_dif = both_src_img - both_dst_img
-    
-    dst_dif = K.filters.gaussian_blur2d(dst_dif.unsqueeze(0), (filter_size, filter_size), (sigma, sigma)).squeeze(0)
-    src_dif = K.filters.gaussian_blur2d(src_dif.unsqueeze(0), (filter_size, filter_size), (sigma, sigma)).squeeze(0)
-    
-    dst_dif = dst_dif.sum(axis=0, keepdim=True)
-    src_dif = src_dif.sum(axis=0, keepdim=True)
-    
-    dst_dif = torch.threshold(dst_dif, 0.35, 0)
-    src_dif = torch.threshold(src_dif, 0.35, 0)
-    
-    dst_dif = torch.any(dst_dif > 0, dim=0, keepdim=True).to(torch.uint16)
-    src_dif = torch.any(src_dif > 0, dim=0, keepdim=True).to(torch.uint16)
-    
-    dst_dif = K.morphology.erosion(dst_dif.unsqueeze(0), torch.ones(erosion_kernel, erosion_kernel)).squeeze(0)
-    src_dif = K.morphology.erosion(src_dif.unsqueeze(0), torch.ones(erosion_kernel, erosion_kernel)).squeeze(0)
-    
-    dst_dif = K.morphology.dilation(dst_dif.unsqueeze(0), torch.ones(dilation_kernel, dilation_kernel)).squeeze(0)
-    src_dif = K.morphology.dilation(src_dif.unsqueeze(0), torch.ones(dilation_kernel, dilation_kernel)).squeeze(0)
-    
-    # dst= both_dst_img * (dst_dif + src_dif)
-    # src = both_src_img * (dst_dif + src_dif)
-    
-    dst= both_dst_img * src_dif 
-    src = both_src_img * dst_dif
-
-    # t1_1 is src, t1_2 is dst
-    # show dst_img if src_dif is 0 else show src_img
-    #dst_mask if src_dif is 0 else src_mask
-    conditional = (src_mask & (src_dif.to(torch.uint8) == 1)) | ~dst_mask
-    combined_mask = torch.where(conditional, src_img, dst_img)
-    
-    conditional2 = (dst_mask & (dst_dif.to(torch.uint8) == 1)) | ~src_mask
-    # combined_mask = torch.where(conditional, dst_img, src_img)
-    
+        imlist = list(imgs.values())
         
+        t1_1 = (imlist[0]/255).unsqueeze(0)
+        t1_2 = (imlist[1]/255).unsqueeze(0)
+        
+        def ready(tens):
+            if len(tens.shape) == 4:
+                tens = tens.squeeze(0)
+            return (tens * 255).to(torch.uint8)
+        
+        def i8(tens):
+            return tens.to(torch.uint8)
+
+        
+        # From Kornia ImageStitcher class source code:
+        if variant:
+            inputdict = {
+            "image0": K.color.rgb_to_grayscale(t1_1),
+            "image1": K.color.rgb_to_grayscale(t1_2)
+            }
+            shp = t1_1.shape
+        else:
+            inputdict = {
+            "image0": K.color.rgb_to_grayscale(t1_2),
+            "image1": K.color.rgb_to_grayscale(t1_1)
+            }
+            shp = t1_2.shape
+        
+        loftr = K.feature.LoFTR(pretrained='outdoor')
+        points = loftr(inputdict)
+        
+
+        ransac = K.geometry.RANSAC(inl_th=inl_th, max_iter=max_iter, confidence=confidence, max_lo_iters=max_lo_iters)
+        homo = ransac(points["keypoints0"], points["keypoints1"])
+        homo = (homo[0],homo[1])
+        def transform(points, H):
+            # Equation 2.21 Szeliski Computer Vision: Algorithms and Applications
+            out = []
+            for point in points:
+                x = point[0]
+                y = point[1]
+                h = H
+                x_prime = (h[0][0]*x + h[0][1]*y + h[0][2])/(h[2][0]*x + h[2][1]*y + h[2][2])
+                y_prime = (h[1][0]*x + h[1][1]*y + h[1][2])/(h[2][0]*x + h[2][1]*y + h[2][2])
+                out.append((int(x_prime.round()), int(y_prime.round())))
+            return tuple(out)
+        # print(shp)
+        
+        tl,tr = (0, 0), (shp[3],0)
+        bl, br = (0,shp[2]), (shp[3], shp[2])
+        points = (tl, tr, bl, br)
+        t = transform(points, homo[0])
+        tl_prim, tr_prim, bl_prim, br_prim = t[0],t[1],t[2],t[3]
+        if variant:
+            x = tl_prim[0], tr_prim[0], bl_prim[0], br_prim[0], t1_2.shape[3], 0
+            y = tl_prim[1], tr_prim[1], bl_prim[1], br_prim[1], t1_2.shape[2], 0
+        else:
+            x = tl_prim[0], tr_prim[0], bl_prim[0], br_prim[0], t1_1.shape[3], 0
+            y = tl_prim[1], tr_prim[1], bl_prim[1], br_prim[1], t1_1.shape[2], 0
+        
+        
+        minx, maxx = int(min(x)), int(max(x))
+        miny, maxy = int(min(y)), int(max(y))
+        
+        width = maxx-minx
+        height = maxy-miny
+        outs = (height, width)
+        
+        # homo needs transform
+        T = torch.tensor([[1, 0, -minx],[0, 1 ,-miny],[0, 0, 1]], dtype=torch.float32)    
+        H = (T @ homo[0]).unsqueeze(0)
+        
+
+        
+        if variant:
+            src_img = K.geometry.warp_perspective(t1_1, H, outs, padding_mode=p, align_corners=align_corners, mode=m) # change mode
+            src_mask = K.geometry.warp_perspective(torch.ones_like(t1_1), H, outs, padding_mode=p, align_corners=align_corners, mode=m) # change mode
+        else:
+            src_img = K.geometry.warp_perspective(t1_2, H, outs, padding_mode=p, align_corners=align_corners, mode=m)
+            src_mask = K.geometry.warp_perspective(torch.ones_like(t1_2), H, outs, padding_mode=p, align_corners=align_corners, mode=m)
+
+        # https://docs.pytorch.org/docs/stable/generated/torch.nn.functional.pad.html 
+        # then use (padding_left,padding_right, padding_top,padding_bottom)
+        # (at least minx,the width + minx, miny, the height + miny)
+        # print(minx,width+minx, miny, height+miny)
+        
+        minx = abs(minx)
+        miny = abs(miny)
+        
+        # print(f"minx: {minx}\nwidth+minx: {width+minx}\nminy: {miny}\nheight+miny: {height+miny}\nright.shape: {t1_2.shape}\nsrc_img.shape: {src_img.shape}\nright.shape[2]-height+miny: {t1_2.shape[2]-height+miny} ")
+        # print(f"minx: {minx}\nwidth+minx: {width+minx}\nminy: {miny}\nheight+miny: {height+miny}\nleft.shape: {t1_1.shape}\nsrc_img.shape: {src_img.shape}\nleft.shape[2]-height+miny: {t1_1.shape[2]-height+miny} ")
+        if variant:
+            dst_img = torch.nn.functional.pad(t1_2, (minx,t1_2.shape[3]-width+minx, miny, height-miny-t1_2.shape[2]))     
+            dst_mask = torch.nn.functional.pad(torch.ones_like(t1_2), (minx,t1_2.shape[3]-width+minx, miny, height-miny-t1_2.shape[2]))     
+        else:
+            dst_img = torch.nn.functional.pad(t1_1, (minx, width-minx-t1_1.shape[3], miny, height-miny-t1_1.shape[2]))     
+            dst_mask = torch.nn.functional.pad(torch.ones_like(t1_1), (minx, width-minx-t1_1.shape[3], miny, height-miny-t1_1.shape[2]))     
+        
+        dst_mask = dst_mask.bool()
+        src_mask = src_mask.bool()
+        
+        # print(src_mask.shape, src_mask.dtype)
+        # print(dst_mask.shape, dst_mask.dtype)
+
+        both = dst_mask & src_mask
+        
+        both_dst_img = dst_img * both.float()
+        both_src_img = src_img * both.float()
+        
+        dst_dif = both_dst_img - both_src_img 
+        src_dif = both_src_img - both_dst_img
+        
+        # save
+        # dst_img, dst_mask, dst_dif
+        # src_img, src_mask, src_dif
+        torch.save(dst_img, f"{save_path}/dst_img.pt")
+        torch.save(dst_mask, f"{save_path}/dst_mask.pt")
+        torch.save(dst_dif, f"{save_path}/dst_dif.pt")
+        torch.save(src_img, f"{save_path}/src_img.pt")
+        torch.save(src_mask, f"{save_path}/src_mask.pt")
+        torch.save(src_dif, f"{save_path}/src_dif.pt")
+
+    if load and not save:
+        dst_img = torch.load(f"{save_path}/dst_img.pt")
+        dst_mask = torch.load(f"{save_path}/dst_mask.pt")
+        dst_dif = torch.load(f"{save_path}/dst_dif.pt")
+        src_img = torch.load(f"{save_path}/src_img.pt")
+        src_mask = torch.load(f"{save_path}/src_mask.pt")
+        src_dif = torch.load(f"{save_path}/src_dif.pt")
     
-    write_image(ready(conditional),"mask.png")
-    write_image(ready(conditional2),"mask_alt.png")
-    write_image(ready(combined_mask), "out.png")
-    write_image(ready(src_img), "src.png")
-    write_image(ready(dst_img), "dst.png")
     
+    alpha = True
+    
+    if alpha:
+        sigma = 6
+        filter_size = 6 * sigma + 1
+        erosion_kernel = 12
+        dilation_kernel = 50
+        dst_dif = K.filters.gaussian_blur2d(dst_dif, (filter_size, filter_size), (sigma, sigma))
+        src_dif = K.filters.gaussian_blur2d(src_dif, (filter_size, filter_size), (sigma, sigma))
+        
+        dst_dif = torch.threshold(dst_dif, 0.1, 0)
+        src_dif = torch.threshold(src_dif, 0.1, 0)
+
+        
+        low_threshold = 0.1
+        high_threshold = 0.2
+        dst_dif = K.filters.canny(dst_dif, low_threshold, high_threshold, (filter_size, filter_size), (sigma, sigma))
+        src_dif = K.filters.canny(src_dif, low_threshold, high_threshold, (filter_size, filter_size), (sigma, sigma))
+        
+
+
+        
+    # return img
+    else:
+        sigma = 6
+        filter_size = 6 * sigma + 1
+        erosion_kernel = 12
+        dilation_kernel = 50
+        dst_dif = K.filters.gaussian_blur2d(dst_dif, (filter_size, filter_size), (sigma, sigma))
+        src_dif = K.filters.gaussian_blur2d(src_dif, (filter_size, filter_size), (sigma, sigma))
+        
+        dst_dif = dst_dif.sum(axis=1, keepdim=True)
+        src_dif = src_dif.sum(axis=1, keepdim=True)
+
+
+        dst_dif = torch.threshold(dst_dif, 0.15, 0)
+        src_dif = torch.threshold(src_dif, 0.15, 0)
+
+
+        dst_dif = torch.any(dst_dif > 0, dim=1, keepdim=True).to(torch.uint16)
+        src_dif = torch.any(src_dif > 0, dim=1, keepdim=True).to(torch.uint16)
+
+
+        dst_dif = K.morphology.erosion(dst_dif, torch.ones(erosion_kernel, erosion_kernel))
+        src_dif = K.morphology.erosion(src_dif, torch.ones(erosion_kernel, erosion_kernel))
+
+        
+        dst_dif = K.morphology.dilation(dst_dif, torch.ones(dilation_kernel, dilation_kernel))
+        src_dif = K.morphology.dilation(src_dif, torch.ones(dilation_kernel, dilation_kernel))
+
+        
+        # t1_1 is src, t1_2 is dst
+        # show dst_img if src_dif is 0 else show src_img
+        #dst_mask if src_dif is 0 else src_mask
+        
+        # combined_mask = (src_mask & (src_dif.to(torch.uint8) == 1)) | ~dst_mask
+        
+        foreground = src_mask & (src_dif.to(torch.uint8) == 1)
+        # foreground = K.morphology.erosion(foreground.to(torch.uint16), torch.ones(10, 10))
+        
+        foreground2 = dst_mask & (dst_dif.to(torch.uint8) == 1)
+        write_image(ready(foreground),"foreground.png")
+        # write_image(ready(foreground2),"foreground2.png")
+        
+        
+        blend_sigma = 4
+        blend_filter_size = 6 * sigma + 1
+        blur_mask = foreground.to(torch.float32)
+        
+        blur_mask = K.filters.gaussian_blur2d(blur_mask, (blend_filter_size, blend_filter_size), (blend_sigma, blend_sigma))
+        blur_mask = K.morphology.erosion(blur_mask, torch.ones(5, 5))
+        # blur_mask = blur_mask * both
+        blur_mask = (~dst_mask).to(torch.float32) + blur_mask
+        
+        # feathering equation from slides
+        # I_blend = alpha * I_left + (1-alpha) * I_right
+        out = blur_mask * src_img + (1-blur_mask) * dst_img
+            
+        write_image(ready(blur_mask),"mask.png")
+        write_image(ready(out), "out.png")
+        write_image(ready(both), "both.png")
+        
+        write_image(ready(dst_mask), "dst_mask.png")
+        write_image(ready(src_mask), "src_mask.png")
+        
+        write_image(ready(src_img), "src.png")
+        write_image(ready(dst_img), "dst.png")
+        
     return img
 
 # ------------------------------------ Task 2 ------------------------------------ #
